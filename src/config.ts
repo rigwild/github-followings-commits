0c9203865bf8869a07587fa17ad80896763aa436
rigwild <contact@asauvage.fr>
2019-01-03 15:48:59 +0100

    more evolutive router system

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
 'use strict'
 
 export const API: String = 'https://api.github.com'
-export const username: String = 'rigwild'
 
-export default { API, username }
+export default { API }
--- src/router.ts
+++ src/router.ts
@@ -3,11 +3,21 @@
 import conf from './config'
 import fetch from 'node-fetch'
 
-// Get all the users the user is following
-const getFollowings = (username: String) =>
-  fetch(`${conf.API}/users/${username}/following`).then(res => res.json())
+const routes: Array<{ regexp: RegExp; fn: Function }> = [
+  {
+    // Get all the users the user is following - /:username/events
+    regexp: /^\/(.*?)\/followings$/,
+    fn: (username: String) =>
+      fetch(`${conf.API}/users/${username}/following`).then(res => res.json())
+  },
+  {
+    // Get the last events of a user - /:username/events
+    regexp: /^\/(.*?)\/events$/,
+    fn: (username: String) =>
+      fetch(`${conf.API}/users/${username}/following`).then(res => res.json())
+  }
+]
 
-// Get the last events of a user
 const getEvents = (username: String) =>
   fetch(`${conf.API}/users/${username}/events`).then(res => res.json())
 
@@ -17,26 +27,21 @@ interface RouterObj {
   params: Array<String> | null
 }
 
-// Get the function corresponding to the route
+// Get an object corresponding to the route
 const router = (url: String): RouterObj => {
-  let res: RouterObj
   let x: Array<String> | null
 
-  if ((x = url.match(/\/(.*?)\/followings/))) {
-    res = {
-      exist: true,
-      fn: getFollowings,
-      params: x.length >= 2 ? x.slice(1) : null
+  for (const aRoute of routes) {
+    if ((x = url.match(aRoute.regexp))) {
+      return {
+        exist: true,
+        fn: aRoute.fn,
+        params: x.length >= 2 ? x.slice(1) : null
+      }
     }
-  } else if ((x = url.match(/\/(.*?)\/events/))) {
-    res = {
-      exist: true,
-      fn: getEvents,
-      params: x.length >= 2 ? x.slice(1) : null
-    }
-  } else res = { exist: false, fn: null, params: null }
+  }
 
-  return res
+  return { exist: false, fn: null, params: null }
 }
 
 export default router
